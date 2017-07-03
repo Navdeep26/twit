e490668a44a2d483ada4cea6c5134b8f57412276
Nawdeep <nawdeep@Hares-MacBook-Pro.local>
2017-07-03 11:23:10 +0530

    cahnge from 10 to 20

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -3,7 +3,7 @@ class TwitterController < ApplicationController
   def index
 
   	query = params[:search].present? ? params[:search] : "#amazon"
-   	@results = CLIENT.search(query, :result_type => "recent").take(10)
+   	@results = CLIENT.search(query, :result_type => "recent").take(30)
      
   end
 

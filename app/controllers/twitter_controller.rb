5e566de79c5c482ba2f2fb7b7d0eaf962ec7a9f4
Nawdeep <nawdeep26@gmail.com>
2017-07-05 02:55:11 +0530

    added screen name, photo as link and fixed fotter

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -4,6 +4,7 @@ class TwitterController < ApplicationController
 
   	query = params[:search].present? ? params[:search] : "#amazon"
    	@results = CLIENT.search(query, :result_type => "recent").take(30)
+   	#render json:results
      
   end
 

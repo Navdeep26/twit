46e05b7c82bfa744223026e7c7cc60cf7fcfdd59
Nawdeep <nawdeep26@gmail.com>
2017-07-15 17:14:32 +0530

    added ajaz

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -3,9 +3,11 @@ class TwitterController < ApplicationController
   def index
 
   	query = params[:search].present? ? params[:search] : "#amazon"
+
+  	logger.info("query")
+
    	@results = CLIENT.search(query, :result_type => "recent").take(30)
    	#render json:results
-     
   end
 
 end

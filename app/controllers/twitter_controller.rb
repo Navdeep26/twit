4c24d753096ab3746cccb30a5388e9769b0cc1c5
Nawdeep <nawdeep26@gmail.com>
2017-07-19 00:36:39 +0530

    fixed view page

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,13 +1,22 @@
 class TwitterController < ApplicationController
-  
+  layout 'application', except: [:search]
+  before_action :common, only: [:index, :search]
+
   def index
+  	
+  end
 
-  	query = params[:search].present? ? params[:search] : "#amazon"
+  def search
+  	render '_twit', layout: false
+  end
 
-  	logger.info("query")
-
+  def common
+  	query = params[:q].present? ? params[:q] : "#amazon"
+  	logger.info(query)
    	@results = CLIENT.search(query, :result_type => "recent").take(30)
-   	#render json:results
   end
 
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    get '/' => 'twitter#index'
    get 'tweets' => 'twitter#index'
+   get 'twit' => 'twitter#search'
 end
